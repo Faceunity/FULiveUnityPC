7b14ed581b9075d1c66fbb0b18762b3c10883efb
陈亨键 <hengjianchen@faceunity.com>
2019-03-14 16:50:14 +0800

    add makeup support

--- Assets/Script/Core/FaceunityWorker.cs
+++ Assets/Script/Core/FaceunityWorker.cs
@@ -134,6 +134,26 @@ public class FaceunityWorker : MonoBehaviour
     public static extern int fu_SetRuningMode(int runningMode);//refer to FURuningMode 
 
     /**
+\brief Create Tex For Item
+\param item specifies the item
+\param name is the tex name
+\param value is the tex rgba buffer to be set ,use GCHandle to get ptr
+\param width is the tex width
+\param height is the tex height
+\return zero for failure, non-zero for success
+*/
+    [DllImport("faceplugin", CallingConvention = CallingConvention.Cdecl)]
+    public static extern int fu_CreateTexForItem(int itemid, [MarshalAs(UnmanagedType.LPStr)]string name, IntPtr value, int width, int height);
+
+    /**
+\brief Delete Tex For Item
+\param item specifies the item
+\param name is the parameter name
+*/
+    [DllImport("faceplugin", CallingConvention = CallingConvention.Cdecl)]
+    public static extern int fu_DeleteTexForItem(int itemid, [MarshalAs(UnmanagedType.LPStr)]string name);
+
+    /**
 \brief Set an item parameter to a double value
 \param item specifies the item
 \param name is the parameter name
